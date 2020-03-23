9e5c19c390c0e222f345f54e4590b4eb9b32a452
Sanket <sbenade96@gmail.com>
2020-03-23 11:53:55 +0530

    2nd assignment Q1.

--- Assignments (23-03-2020)/highestNumber/Program.cs
+++ Assignments (23-03-2020)/highestNumber/Program.cs
@@ -1,15 +1,69 @@
-﻿using System;
+﻿//C# program to find largest number from 3 numbers
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace highestNumber
+namespace Assignments__23_3_2020_
 {
     class Program
     {
+        static int num1, num2, num3, max;
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Enter Num1 :");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Num2 :");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Num3 :");
+            num3 = int.Parse(Console.ReadLine());
+
+
+            #region highest number using ternary operator
+
+            /*max = num1 > num2 ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+            Console.WriteLine($"Maximum number from 3 numbers is {max}");*/
+
+            #endregion
+
+            #region highest number using simple if-else
+
+            /*if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine($"Max num is {num1}");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine($"Max num is {num2}");
+            }
+            else
+                Console.WriteLine($"Max number is {num3}");*/
+            #endregion
+
+            #region highest number using nested if else
+            /*if (num1 > num2)
+            {
+                if (num1 > num3)
+                    max = num1;
+                else
+                    max = num3;
+            }
+            else if (num2 > num3)
+            {
+                max = num2;
+            }
+            else
+                max = num3;
+
+            Console.WriteLine($"max number is {max}");*/
+            #endregion
+
+
         }
     }
 }
